5f6d998a402b99fb4d55ae8b5067bae4bf2a61c7
Nina Hamidli <nigar.hamidli1999@gmail.com>
2021-03-13 17:14:00 +0900

    Fixed fav button

--- Twitter/TweetCellTableViewCell.swift
+++ Twitter/TweetCellTableViewCell.swift
@@ -9,10 +9,10 @@
 import UIKit
 
 class TweetCellTableViewCell: UITableViewCell {
-
+    
     var favorited:Bool = false
     var tweetId: Int = -1
-    
+
     @IBOutlet weak var profileImageView: UIImageView!
 
     @IBOutlet weak var userNameLabel: UILabel!
@@ -30,30 +30,41 @@ class TweetCellTableViewCell: UITableViewCell {
     @IBAction func retweet(_ sender: Any) {
     }
     @IBAction func favTweet(_ sender: Any) {
-        let toBeFavorited = !favorited
-        if (toBeFavorited) {
-            TwitterAPICaller.client?.favoriteTweet(tweetId: tweetId, success: { self.setFavorite(true) }, failure: { (error) in
-                print("fav did not succeed \(error)")
-            })
-        } else {
-            TwitterAPICaller.client?.unfavoriteTweet(tweetId: tweetId, success: { self.setFavorite(false) }, failure: { (error) in
-                print("Unfav did not succeed \(error)")
-            })
-        }
+    let toBeFavorited = !favorited
+            if (toBeFavorited)
+            {
+                TwitterAPICaller.client?.favoriteTweet(tweetId:tweetId, success: {
+                    self.setFavorite(true)
+                
+                }, failure: { (error) in
+                    print("Favorite did not succeed \(error)")
+                })
+            }
             
-    }
+            else{
+                TwitterAPICaller.client?.unFavoriteTweet(tweetId: tweetId, success: {
+                    self.setFavorite(false)
+                }, failure: { (error) in
+                    print("Unfavorite did not succeed \(error)" )
+                })
+            }
+        }
     
     
-    func setFavorite (_ isFavorited:Bool) {
-        favorited = isFavorited
-        
-        if (favorited) {
-            favButton.setImage(UIImage(named:"favor-icon-red"), for: UIControl.State.normal)
+    func setFavorite(_ isFavorited:Bool)
+        {
+            favorited = isFavorited
+            if (favorited)
+            {
+                favButton.setImage(UIImage(named:"favor-icon-red"), for: UIControl.State.normal)
+            }
+            else{
+                 favButton.setImage(UIImage(named:"favor-icon"), for: UIControl.State.normal)
+            }
+            
+            
+            
         }
-        else {
-            favButton.setImage(UIImage(named:"favor-icon"), for: UIControl.State.normal)
-        }
-    }
     
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
--- Twitter/TwitterAPICaller.swift
+++ Twitter/TwitterAPICaller.swift
@@ -74,24 +74,35 @@ class TwitterAPICaller: BDBOAuth1SessionManager {
         
     }
     
-    func favoriteTweet(tweetId: Int, success: @escaping () -> (), failure: @escaping (Error) -> ()) {
+    func favoriteTweet(tweetId:Int, success: @escaping () -> (), failure:@escaping (Error) ->()){
+        
         let url = "https://api.twitter.com/1.1/favorites/create.json"
-        TwitterAPICaller.client?.post(url, parameters: ["is":tweetId], progress: nil, success: { (task: URLSessionDataTask, response: Any?) in
+        TwitterAPICaller.client?.post(url, parameters: ["id":tweetId], progress: nil, success: { (task: URLSessionDataTask, response: Any?) in
             success()
         }, failure: { (task: URLSessionDataTask?, error: Error) in
             failure(error)
         })
-    
     }
     
-    func unfavoriteTweet(tweetId: Int, success: @escaping () -> (), failure: @escaping (Error) -> ()) {
-        let url = "https://api.twitter.com/1.1/favorites/destroy.json"
-        TwitterAPICaller.client?.post(url, parameters: ["is":tweetId], progress: nil, success: { (task: URLSessionDataTask, response: Any?) in
+    
+    func unFavoriteTweet(tweetId:Int, success: @escaping () -> (), failure:@escaping (Error) ->()){
+         
+         let url = "https://api.twitter.com/1.1/favorites/destroy.json"
+         TwitterAPICaller.client?.post(url, parameters: ["id":tweetId], progress: nil, success: { (task: URLSessionDataTask, response: Any?) in
+             success()
+         }, failure: { (task: URLSessionDataTask?, error: Error) in
+             failure(error)
+         })
+     }
+    
+    func reTweet(tweetId:Int, success: @escaping () -> (), failure:@escaping (Error) ->()){
+        
+        let url = "https://api.twitter.com/1.1/statuses/retweet/\(tweetId).json"
+        TwitterAPICaller.client?.post(url, parameters: ["id":tweetId], progress: nil, success: { (task: URLSessionDataTask, response: Any?) in
             success()
         }, failure: { (task: URLSessionDataTask?, error: Error) in
             failure(error)
         })
-    
     }
     
 }
